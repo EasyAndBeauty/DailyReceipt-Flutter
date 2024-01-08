405d3bb2465372dd16043771cc0ace5a4e38fd02
jayjeong8 <zuni4242@gmail.com>
2024-01-08 21:35:29 +0900

    feat: text theme 구현
    - 일관된 톤앤매너를 유지하기 위해 text theme을 추가했습니다.
    - courierPrime: 영수증 폰트 - 영수증 및 버튼 text에 사용됩니다.
    - gothicA1: 한글 및 영어가 둘다 괜찮은 고딕 폰트 - todo text 및 날짜 UI, 한국어 text에 사용됩니다.
    - 참고: https://api.flutter.dev/flutter/material/TextTheme-class.html

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,22 +14,50 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: const ColorScheme(
-            brightness: Brightness.dark,
-            primary: Color(0xFF191919),
-            onPrimary: Color(0xFFefefef),
-            secondary: Color(0xFFaaaaaa),
-            onSecondary: Color(0xFF191919),
-            tertiary: Color(0xFF81C944),
-            onTertiary: Color(0xFFefefef),
-            error: Color(0xFFA65C5C),
-            onError: Color(0xFFefefef),
-            background: Color(0xFF191919),
-            onBackground: Color(0xFFefefef),
-            surface: Color(0xFFefefef),
-            onSurface: Color(0xFF191919),
-          )),
+        useMaterial3: true,
+        colorScheme: const ColorScheme(
+          brightness: Brightness.dark,
+          primary: Color(0xFF191919),
+          onPrimary: Color(0xFFefefef),
+          secondary: Color(0xFFaaaaaa),
+          onSecondary: Color(0xFF191919),
+          tertiary: Color(0xFF81C944),
+          onTertiary: Color(0xFFefefef),
+          error: Color(0xFFA65C5C),
+          onError: Color(0xFFefefef),
+          background: Color(0xFF191919),
+          onBackground: Color(0xFFefefef),
+          surface: Color(0xFFefefef),
+          onSurface: Color(0xFF191919),
+        ),
+        textTheme: TextTheme(
+          headlineLarge: GoogleFonts.gothicA1(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+          titleLarge: GoogleFonts.courierPrime(
+            fontSize: 24,
+            letterSpacing: -1.5,
+          ),
+          titleMedium: GoogleFonts.courierPrime(
+            fontSize: 18,
+            letterSpacing: -1.5,
+          ),
+          titleSmall: GoogleFonts.courierPrime(
+            fontSize: 14,
+            letterSpacing: -1.5,
+          ),
+          bodyLarge: GoogleFonts.gothicA1(
+            fontSize: 24,
+          ),
+          bodyMedium: GoogleFonts.gothicA1(
+            fontSize: 16,
+          ),
+          bodySmall: GoogleFonts.gothicA1(
+            fontSize: 14,
+          ),
+        ),
+      ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
