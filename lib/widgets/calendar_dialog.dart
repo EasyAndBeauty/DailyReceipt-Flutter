99e4eaeff1db812b2d7ed3f8301b41046c6f6634
jayjeong8 <zuni4242@gmail.com>
2024-01-21 20:27:52 +0900

    feat: 캘린더 날짜영역 스타일 개선
    - Dialog의 insetPadding을 변경해서 dialog가 더 넓게 나타나도록 dialog와 전체 화면 사이의 여백을 줄였습니다.
    - SizedBox로 세로 높이를 지정했습니다.
    - 날짜영역 padding 및 style, 간격을 조정했습니다.
    - 오늘 날짜에 검은색 원으로 표시해서 구분할 수 있도록 했습니다.

--- lib/widgets/calendar_dialog.dart
+++ lib/widgets/calendar_dialog.dart
@@ -10,29 +10,44 @@ class CalendarDialog extends StatelessWidget {
     return Dialog(
       backgroundColor: Theme.of(context).colorScheme.surface,
       surfaceTintColor: Theme.of(context).colorScheme.surface,
+      insetPadding: const EdgeInsets.symmetric(horizontal: 20.0),
       child: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: TableCalendar(
-          firstDay: DateTime.utc(2023, 1, 1),
-          lastDay: DateTime.utc(2034, 12, 31),
-          focusedDay: DateTime.now(),
-          calendarFormat: CalendarFormat.month,
-          headerStyle: HeaderStyle(
-            formatButtonVisible: false,
-            titleCentered: true,
-            titleTextFormatter: (date, locale) {
-              return DateFormat('yyyy.MM.').format(date.toLocal()).toString();
-            },
-            titleTextStyle: Theme.of(context).textTheme.titleLarge!,
-            headerPadding: const EdgeInsets.only(top: 4, bottom: 12),
-            leftChevronIcon: Icon(
-              Icons.chevron_left,
-              color: Theme.of(context).colorScheme.secondary,
+        child: SizedBox(
+          height: 420,
+          child: TableCalendar(
+            firstDay: DateTime.utc(2023, 1, 1),
+            lastDay: DateTime.utc(2034, 12, 31),
+            focusedDay: DateTime.now(),
+            calendarFormat: CalendarFormat.month,
+            headerStyle: HeaderStyle(
+              formatButtonVisible: false,
+              titleCentered: true,
+              titleTextFormatter: (date, locale) {
+                return DateFormat('yyyy.MM.').format(date.toLocal()).toString();
+              },
+              titleTextStyle: Theme.of(context).textTheme.titleLarge!,
+              headerPadding: const EdgeInsets.only(top: 6, bottom: 6),
+              leftChevronIcon: Icon(
+                Icons.chevron_left,
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+              rightChevronIcon: Icon(
+                Icons.chevron_right,
+                color: Theme.of(context).colorScheme.secondary,
+              ),
             ),
-            rightChevronIcon: Icon(
-              Icons.chevron_right,
-              color: Theme.of(context).colorScheme.secondary,
+            calendarStyle: CalendarStyle(
+              tablePadding: const EdgeInsets.all(8),
+              todayTextStyle: TextStyle(
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+              todayDecoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+                shape: BoxShape.circle,
+              ),
             ),
+            daysOfWeekHeight: 30,
           ),
         ),
       ),
