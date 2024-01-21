4f64b56308b48188f5e87371e225afc3064c86ea
jayjeong8 <zuni4242@gmail.com>
2024-01-21 20:00:42 +0900

    feat: dialog에 캘린더 추가
    - firstDay, lastDay로 총 날짜 구간을 설정합니다.
    - calendarFormat을 월단위로 설정합니다. month, twoWeeks, week 중 선택이 가능합니다.
    - focusedDay에 설정한 날짜에  원으로 focused 스타일이 적용됩니다.

--- lib/widgets/calendar_dialog.dart
+++ lib/widgets/calendar_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarDialog extends StatelessWidget {
   const CalendarDialog({super.key});
@@ -6,8 +7,17 @@ class CalendarDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Dialog(
-      child: Text('dialog'),
       backgroundColor: Theme.of(context).colorScheme.surface,
+      surfaceTintColor: Theme.of(context).colorScheme.surface,
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: TableCalendar(
+          firstDay: DateTime.utc(2023, 1, 1),
+          lastDay: DateTime.utc(2034, 12, 31),
+          focusedDay: DateTime.now(),
+          calendarFormat: CalendarFormat.month,
+        ),
+      ),
     );
   }
 }
