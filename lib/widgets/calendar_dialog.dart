b97b3c8ac2d25d836271f73f0897ed1d51c7ad2e
jayjeong8 <zuni4242@gmail.com>
2024-01-21 20:06:19 +0900

    feat: 캘린더 headerStyle 개선
    - titleTextStyle에 설정한 textTheme.titleLarge가 nullable로 인식되어 발생하는 에러를 해결하기 위해 !를 붙였습니다.
    - formatButtonVisible을 false로 설정해서 사용하지 않는 포맷 버튼이 나타나지 않도록 했습니다.
    - titleTextFormatter로 date가 2024.01. 와 같이 표현되도록 했습니다.
    - 선택된 달을 변경하는 chevron 버튼의 색상을 변경했습니다.

--- lib/widgets/calendar_dialog.dart
+++ lib/widgets/calendar_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class CalendarDialog extends StatelessWidget {
@@ -16,6 +17,23 @@ class CalendarDialog extends StatelessWidget {
           lastDay: DateTime.utc(2034, 12, 31),
           focusedDay: DateTime.now(),
           calendarFormat: CalendarFormat.month,
+          headerStyle: HeaderStyle(
+            formatButtonVisible: false,
+            titleCentered: true,
+            titleTextFormatter: (date, locale) {
+              return DateFormat('yyyy.MM.').format(date.toLocal()).toString();
+            },
+            titleTextStyle: Theme.of(context).textTheme.titleLarge!,
+            headerPadding: const EdgeInsets.only(top: 4, bottom: 12),
+            leftChevronIcon: Icon(
+              Icons.chevron_left,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            rightChevronIcon: Icon(
+              Icons.chevron_right,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
         ),
       ),
     );
