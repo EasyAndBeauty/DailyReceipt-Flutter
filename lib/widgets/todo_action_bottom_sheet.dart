4af70f191db781e5e46dc45007bee9b684c299b1
sangwooyang <dndb3599@gmail.com>
2024-10-06 01:00:54 +0900

    feat(action_bottom): paper 스타일 넣기

--- lib/widgets/todo_action_bottom_sheet.dart
+++ lib/widgets/todo_action_bottom_sheet.dart
@@ -21,45 +21,62 @@ class TodoActionBottomSheet extends StatelessWidget {
 
     return ClipPath(
       clipper: ReceiptTopClipper(), // Clipper 적용
-      child: Material(
-        color: theme.colorScheme.surface,
-        child: SafeArea(
-          top: false,
-          child: Padding(
-            padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                _buildTitle(theme),
-                const SizedBox(height: 8),
-                _buildDashedLine(theme),
-                Row(children: [
-                  Expanded(
-                    child: EditButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                        onEdit();
-                      },
-                      color: theme.colorScheme.primary, // Edit 버튼 색상
+      child: Stack(
+        children: [
+          _buildBackgroundImage(), // 배경 이미지 추가
+          Material(
+            color: Colors.transparent, // 투명한 색상으로 설정
+            child: SafeArea(
+              top: false,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    _buildTitle(theme),
+                    const SizedBox(height: 8),
+                    _buildDashedLine(theme),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: EditButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                              onEdit();
+                            },
+                            color: theme.colorScheme.primary, // Edit 버튼 색상
+                          ),
+                        ),
+                        const SizedBox(width: 16),
+                        Expanded(
+                          child: DeleteButton(
+                            onPressed: () {
+                              Provider.of<Todos>(context, listen: false)
+                                  .remove(todo.id);
+                              Navigator.pop(context);
+                            },
+                            color: theme.colorScheme.error, // Delete 버튼 색상
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                  const SizedBox(width: 16),
-                  Expanded(
-                    child: DeleteButton(
-                      onPressed: () {
-                        Provider.of<Todos>(context, listen: false)
-                            .remove(todo.id);
-                        Navigator.pop(context);
-                      },
-                      color: theme.colorScheme.error, // Delete 버튼 색상
-                    ),
-                  ),
-                ])
-              ],
+                  ],
+                ),
+              ),
             ),
           ),
-        ),
+        ],
+      ),
+    );
+  }
+
+  // 배경 이미지 위젯
+  Widget _buildBackgroundImage() {
+    return Positioned.fill(
+      child: Image.asset(
+        'assets/paper_texture.webp', // 이미지 경로
+        fit: BoxFit.cover, // 이미지가 전체 배경을 덮도록 설정
       ),
     );
   }
