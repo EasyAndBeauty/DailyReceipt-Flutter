326ddd4e4e79d58e43edc79f8721d0233b8ec2dc
Sang Woo Yang (Dan) <79236624+IGhost-P@users.noreply.github.com>
2024-11-21 01:58:37 +0900

    feat(front): login 화면 구성

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -4,6 +4,7 @@ import 'package:daily_receipt/services/social_login_service.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:daily_receipt/services/localization_service.dart';
 import 'package:daily_receipt/widgets/dashed_line_painter.dart';
+import 'package:go_router/go_router.dart';
 
 class LoginScreen extends StatelessWidget {
   // final SocialLoginService _socialLoginService = SocialLoginService();
@@ -13,10 +14,10 @@ class LoginScreen extends StatelessWidget {
     return const Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 24),
           child: Column(
             children: [
-              Spacer(flex: 2),
+              Spacer(flex: 1),
               LogoSection(),
               Spacer(flex: 1),
               DashedLine(),
@@ -46,17 +47,14 @@ class LogoSection extends StatelessWidget {
         children: [
           Text(
             'GET STARTED',
-            style: theme.textTheme.titleLarge?.copyWith(
-              color: theme.colorScheme.onSurface,
+            style: theme.textTheme.headlineLarge?.copyWith(
+              color: theme.colorScheme.secondary,
             ),
           ),
           const SizedBox(height: 4),
-          Text(
-            tr.key1,
-            style: theme.textTheme.titleLarge?.copyWith(
-              color: theme.colorScheme.onSurface,
-            ),
-          ),
+          Text(tr.key1,
+              style: theme.textTheme.titleMedium
+                  ?.copyWith(color: theme.colorScheme.onSurface)),
           const SizedBox(height: 4),
           SvgPicture.asset(
             'assets/icons/icon.svg',
@@ -82,6 +80,7 @@ class SocialButtonsSection extends StatelessWidget {
         SocialLoginButton(
           onPressed: () {
             print('Google 로그인 성공');
+            GoRouter.of(context).go('/');
           },
           text: 'Continue with Google',
         ),
@@ -89,6 +88,7 @@ class SocialButtonsSection extends StatelessWidget {
         SocialLoginButton(
           onPressed: () {
             print('Apple 로그인 성공');
+            GoRouter.of(context).go('/');
           },
           text: 'Continue with Apple',
         ),
@@ -111,15 +111,17 @@ class LocaleLoginButton extends StatelessWidget {
         SocialLoginButton(
           onPressed: () {
             print('loacl 로그인 성공');
+            GoRouter.of(context).go('/');
           },
           text: 'Continue with Local',
         ),
         const SizedBox(height: 16),
         Text(
           'if you don’t have an account,\nusing only local features',
-          style: theme.textTheme.titleMedium?.copyWith(
-            color: theme.colorScheme.onSurface,
+          style: theme.textTheme.titleSmall?.copyWith(
+            color: theme.colorScheme.secondary,
           ),
+          textAlign: TextAlign.center,
         ),
       ],
     );
@@ -151,7 +153,7 @@ class SocialLoginButton extends StatelessWidget {
           backgroundColor: theme.colorScheme.surface,
           elevation: 0,
           side: BorderSide.none,
-          padding: const EdgeInsets.symmetric(horizontal: 24),
+          padding: EdgeInsets.zero,
         ),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween, // 텍스트와 화살표를 양끝으로
