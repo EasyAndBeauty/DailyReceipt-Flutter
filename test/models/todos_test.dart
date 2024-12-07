a786af409bfe9fcf08e3077167f2f7a751a75ea7
Madang Garden <chooi9522@gmail.com>
2024-12-08 00:44:25 +0900

    todo 모델 수정 및 svg import 유효화

--- test/models/todos_test.dart
+++ test/models/todos_test.dart
@@ -7,7 +7,7 @@ void main() {
 
     test('새로운 todo가 todos에 추가되어야 합니다.', () {
       var newTodo = Todo(
-        id: todosProvider.todos.length,
+        id: '1',
         content: 'test',
         createdAt: DateTime.now().toUtc(),
         scheduledDate: DateTime(2024, 1, 1),
@@ -23,7 +23,7 @@ void main() {
       final notExistsDate = DateTime(2024, 1, 2);
       var todos = [
         Todo(
-          id: 1,
+          id: '1',
           content: 'test',
           createdAt: DateTime.now().toUtc(),
           scheduledDate: existsDate,
