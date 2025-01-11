5f753e41c445d91703bb9a744ef657080a509cda
Sang Woo Yang (Dan) <79236624+IGhost-P@users.noreply.github.com>
2025-01-11 10:30:21 +0900

    feat(dio): http -> dio 라이브러리로 변경

--- lib/config/di.dart
+++ lib/config/di.dart
@@ -1,9 +1,11 @@
 import 'package:daily_receipt/services/social_login_service.dart';
 import 'package:daily_receipt/services/token_service.dart';
+import 'package:daily_receipt/services/dio_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import '../services/auth_service.dart';
-import 'app_config.dart';
+import 'package:daily_receipt/services/auth_service.dart';
+import 'package:daily_receipt/config/app_config.dart';
 
 final GetIt getIt = GetIt.instance;
 
@@ -15,11 +17,19 @@ Future<void> setupDI() async {
   final sharedPrefs = await SharedPreferences.getInstance();
 
   // TokenService
-  getIt.registerSingleton<TokenService>(TokenService(sharedPrefs));
+  getIt.registerSingleton<TokenService>(TokenService(sharedPrefs, auth: FirebaseAuth.instance));
+
+  // DioService
+  getIt.registerSingleton<DioService>(DioService(
+    baseUrl: AppConfig.instance.baseUrl,
+    tokenService: getIt.get<TokenService>(),
+  ));
 
   // AuthService
   getIt.registerSingleton<AuthService>(
-    AuthService(baseUrl: AppConfig.instance.baseUrl),
+    AuthService(
+      dioService: getIt.get<DioService>(),
+    ),
   );
 
   // SocialLoginService
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,29 +1,24 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:daily_receipt/services/dio_service.dart';
 
 class AuthService {
-  final String baseUrl;
+  final DioService _dioService;
 
-  AuthService({required this.baseUrl});
+  AuthService({
+    required DioService dioService,
+  }) : _dioService = dioService;
 
   Future<Map<String, dynamic>> fetchUserInfo(String idToken) async {
     try {
-      final response = await http.post(
-        Uri.parse('$baseUrl/api/auth/social-auth/'),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: jsonEncode({
+      final response = await _dioService.post(
+        '/api/auth/social-auth/',
+        data: {
           'id_token': idToken,
-        }),
+        },
       );
 
-      if (response.statusCode == 200) {
-        return jsonDecode(response.body);
-      } else {
-        final error = jsonDecode(response.body)['error'];
-        throw Exception('Failed to fetch user info: $error');
-      }
+      print('✅ response: ${response.data}');
+
+      return response.data;
     } catch (e) {
       print('Error fetching user info: $e');
       rethrow;
@@ -33,14 +28,11 @@ class AuthService {
   // 토큰 유효성 검증을 위한 메서드 추가
   Future<bool> validateToken(String idToken) async {
     try {
-      final response = await http.post(
-        Uri.parse('$baseUrl/api/auth/validate-token/'),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: jsonEncode({
+      final response = await _dioService.post(
+        '/api/auth/validate-token/',
+        data: {
           'id_token': idToken,
-        }),
+        },
       );
 
       return response.statusCode == 200;
